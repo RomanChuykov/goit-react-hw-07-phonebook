d0f709ff3cf94c58f1c9f1a23ef7fb28ff9509a6
RomanChuykov <95050528+RomanChuykov@users.noreply.github.com>
2023-11-13 22:30:35 +0200

    1

---
+++ src/components/Filter/Filter.styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+export const Wrapper = styled.div`
+  margin: 0 auto;
+  margin-bottom: 30px;
+  max-width: 50%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+`;
+
+export const Label = styled.label`
+  margin-bottom: 10px;
+  font-weight: 500;
+  font-size: 24px;
+  color: #858585;
+ 
+`;
+
+export const Input = styled.input`
+  padding: 5px 10px;
+  width: 100%;
+
+  font-size: 24px;
+  color: #282828;
+  background-color: transparent;
+  border: 1px solid #282828;
+  border-radius: 5px;
+
+`;
---
+++ src/redux/contactsOperations.js
@@ -0,0 +1,42 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+//  axios.defaults.baseURL = 'https://639efa887aaf11ceb88f70ea.mockapi.io/api/v1';
+ axios.defaults.baseURL = 'https://6552814d5c69a779032a20b4.mockapi.io/contacts/';
+ 
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchContacts',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get('/contacts');
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async ({ name, phone }, { rejectWithValue }) => {
+    try {
+      const response = await axios.post('/contacts', { name, phone });
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (contactId, { rejectWithValue }) => {
+    try {
+      const response = await axios.delete(`/contacts/${contactId}`);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
